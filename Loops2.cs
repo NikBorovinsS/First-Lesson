4a73ed51e67046cf421a95851c4d0faa6680a924
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-04-15 16:44:05 +0300

    Upd1

--- Loops2.cs
+++ Loops2.cs
@@ -1,4 +1,7 @@
-﻿using System;
+//Author: Borovinskiy Nikita
+//Пользователь вводит число вывесли на экран количество разрядов в этом числе
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
