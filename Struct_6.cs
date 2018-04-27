fa705cace568289cbb18c8100328d24dad11b4dc
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-04-27 12:41:33 +0300

    Borovinskiy
    Задание 6
    Создайте перечисление, в котором будут содержаться должности сотрудников как имена констант.
    Присвойте каждой константе значение, задающее количество часов, которые должен отработать сотрудник за
    месяц.
    Создайте класс Accauntant с методом bool AskForBonus(Post worker, int hours), отражающее давать
    или нет сотруднику премию. Если сотрудник отработал больше положеных часов в месяц, то ему положена
    премия.

---
+++ Struct_6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_6
+{
+    class Struct_6
+    {
+        enum Post { Director = 50, Manager = 40, Driver = 30, Cleaner = 20 };
+
+        class Accountant
+        {
+            bool AskForBonus(Post worker, int hours)
+            {
+                return hours > (int)worker;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
