b544c1013f9245eec5f82f80ac3f1c196881575a
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-04-27 12:40:45 +0300

    Borovinskiy
    Задание 3
    Создайте класс MyClass и структуру MyStruct, которые содержат в себе поля public string change.
    В классе Program создайте два метода:
    - static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра myClass
    значение «изменено».
    - static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра myStruct
    значение «изменено».

---
+++ Struct_3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_3
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+    }
+
+    class Struct_3
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "Изменено";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "Изменено";
+        }
+
+        static void Main(string[] args)
+        {
+            MyStruct myStruct = new MyStruct();
+            myStruct.change = "Не изменено";
+
+            MyClass myClass = new MyClass();
+            myClass.change = "Не изменено";
+
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+
+            Console.WriteLine("{0}\n", myClass.change);
+            Console.WriteLine("{0}\n", myStruct.change);
+            Console.ReadKey();
+        }
+    }
+}
