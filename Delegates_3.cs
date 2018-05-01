6528e883972444b628b433340456cc66957e6b95
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-05-01 12:32:24 +0300

    Borovinskiy
    Создайте анонимный метод, который принимает в качестве аргумента массив делегатов и возвращает среднее
    арифметическое возвращаемых значений методов сообщенных с делегатами в массиве. Методы, сообщенные с
    делегатами из массива, возвращают случайное значение типа int.

---
+++ Delegates_3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_3
+{
+    static class MyClass
+    {
+        public static int RandomValue()
+        {
+            Random rng = new Random();
+            return rng.Next(1,100);
+        }
+    }
+
+    public delegate int MyDelegate();
+    public delegate int MidValue(MyDelegate[] mas);
+
+    class Delegates_3
+    {
+        const int MasLength = 5;
+
+        static void Main(string[] args)
+        {
+            MyDelegate[] DelegateMas = new MyDelegate[MasLength];
+            for (int i = 0; i < MasLength; ++i)
+            {
+                DelegateMas[i] = new MyDelegate(MyClass.RandomValue);
+            }
+
+            MidValue MidValDelegate = delegate(MyDelegate[] MD)
+            {
+                int temp = 0;
+                for (int i = 0; i < MasLength; ++i)
+                {
+                    temp+=MD[i].Invoke();
+                }
+                return temp / MasLength;
+            };
+
+            int midValue = MidValDelegate(DelegateMas);
+
+            Console.WriteLine("Среднее арифметическое: {0}\n", midValue);
+            Console.ReadKey();
+        }
+    }
+}
