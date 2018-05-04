87718a8dae3033eea5b8c719e96797ca40fdb86c
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-05-04 20:45:43 +0300

    Borovinskiy
    Создайте класс Dictionary<TKey,TValue>. Реализуйте в простейшем приближении возможность
    использования его экземпляра аналогично экземпляру класса Dictionary из пространства имен
    System.Collections.Generic. Минимально требуемый интерфейс взаимодействия с экземпляром, должен
    включать метод добавления пар элементов, индексатор для получения значения элемента по указанному
    индексу и свойство только для чтения для получения общего количества пар элементов.

---
+++ Generics_6.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_6
+{
+    class Dictionary<TKey, TValue>:IDictionary<TKey, TValue>
+    {
+        class Value
+        {
+            public TKey Key;
+            private TValue Value;
+
+            public Value(TKey NKey, TValue NValue)
+            {
+                Key = NKey;
+                Value = NValue;
+            }
+
+            public TValue GetValue()
+            {
+                return Value;
+            }
+        }
+
+        private List<Value> _collection;
+
+        public Dictionary()
+        {
+            _collection = new List<Value>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _collection.Add(new Value(key, value));
+        }
+
+        public int Count
+        {
+            get { return _collection.Count; }
+        }
+
+        public TValue this[int index]
+        {
+            get
+            {
+                return _collection[index].GetValue();
+            }
+        }
+    }
+
+    class Generics_6
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
