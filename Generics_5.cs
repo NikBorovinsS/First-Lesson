e3bc4cbea3847756e53503f0ecad1b9a3df404bd
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-05-04 20:43:55 +0300

    Borovinskiy
    Создайте класс CarCollection<T>. Реализуйте в простейшем приближении возможность использования его
    экземпляра для создания парка машин. Минимально требуемый интерфейс взаимодействия с экземпляром,
    должен включать метод добавления машин с названием машины и года ее выпуска, индексатор для получения
    значения элемента по указанному индексу и свойство только для чтения для получения общего количества
    элементов.
    Создайте метод удаления всех машин автопарка.

---
+++ Generics_5.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Generics_5
+{
+    class Car
+    {
+        private string _CarName;
+        private string _ReleaseYear;
+
+        public Car(string Name, string Year)
+        {
+            _CarName = Name;
+            _ReleaseYear = Year;
+        }
+
+        public string GetCarInfo()
+        {
+            return _CarName + " " + _ReleaseYear;
+        }
+    }
+    class CarCollection<T>
+    {
+        private ArrayList _Cars;
+
+        public CarCollection()
+        {
+            _Cars = new ArrayList();
+        }
+
+        public void Addcar(T Car)
+        {
+            _Cars.Add(Car);
+        }
+
+        public T this[int index]
+        {
+            get 
+            {
+                return (T)_Cars[index];
+            }
+        }
+
+        public int CountOfCars
+        {
+            get { return _Cars.Count; }
+        }
+
+        public void DeleteAllCars()
+        {
+            _Cars.Clear();
+        }
+    }
+
+    class Generics_5
+    {
+        static void Main(string[] args)
+        {
+            CarCollection<Car> Parking = new CarCollection<Car>();
+
+            Parking.Addcar(new Car("Pontiac GTO", "1967"));
+
+            Console.WriteLine(Parking[0].GetCarInfo());
+
+            Console.WriteLine(Parking.CountOfCars);
+
+            Parking.DeleteAllCars();
+        }
+    }
+}
