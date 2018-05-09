9946d9d488c2676d8ec998c1166caefc5f9f7da5
NikBorovinsS <38322942+NikBorovinsS@users.noreply.github.com>
2018-05-09 09:26:03 +0300

    Borovinskiy
    Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их.

---
+++ IO_1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO_1
+{
+    class IO_1
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\TestDir";
+            string subpath;
+
+            const int maxFolder = 99;
+
+            DirectoryInfo DI = new DirectoryInfo(path);
+
+            if (!DI.Exists)
+            {
+                DI.Create();
+            }
+
+            for (int i = 0; i <= maxFolder; ++i)
+            {
+                subpath = "Folder_" + i.ToString();
+                DI.CreateSubdirectory(subpath);
+            }
+            Console.ReadKey();
+
+            DI.Delete(true);
+        }
+    }
+}
